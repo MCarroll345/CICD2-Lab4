7c010400af05d576abe337a985f234edc512a3a5
MARK CARROLL - STUDENT <g00419267@atu.ie>
2025-10-13 13:54:36 +0100

    Cleanup

--- tests/test_health.py
+++ tests/test_health.py
@@ -2,7 +2,6 @@
 import pytest
 
 def test_health(client):
-    #makes a get request that should assert 200 and return a json format status
     result = client.get("/health")
     assert result.status_code == 200
     assert result.json() == {"status": "ok"}
